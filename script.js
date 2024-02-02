a443ec1fdbd33a7657441bd714f2bafca8e9e72b
Juan Ledesma <barbotigger@hotmail.com.ar>
2024-02-02 09:31:46 -0300

    segundo commit actualizacion

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ let video = document.querySelector("video");
 
 const reproducir = () => {
   video.play(); //para iniciar la reproduccion
-  console.dir(video);
+
   let zonaTiempoReal = document.querySelector("#tiempoReal");
   muestraDurActual = setInterval(() => {
-    console.log("re reproduce la actualizacion del tiempo real");
     zonaTiempoReal.textContent = transformarTiempo(video.currentTime);
   }, 1000);
 };
@@ -24,7 +23,6 @@ const transformarTiempo = (tiempo) => {
     return `00:${tiempo.toFixed(0)}`;
     //metodo toFixed(cant. decimales)
   } else {
-    console.log(tiempo / 60);
     let minutos = parseInt(tiempo / 60);
     let segundos = (tiempo / 60 - minutos) * 60;
     return `${minutos}:${segundos.toFixed(0)}`;
